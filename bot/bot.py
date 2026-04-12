254fc2af31fc43f2fcffdb3c8a75acfdbb6f9630
EagleGamerCoder <natedennis001@gmail.com>
2026-04-12 18:12:25 +0100

    V 0.3.2

--- bot/bot.py
+++ bot/bot.py
@@ -26,8 +26,6 @@ class Class_bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.members = True
-        intents.message_content = True
-        intents.presences = True
 
         super().__init__(command_prefix='/', intents=intents)
         
